6d51bb8e1f57c4b45150796a4615549a0b25b155
ri87nix <maximilian.weigert@stat.uni-muenchen.de>
2020-12-14 13:11:36 +0100

    Update figures and models

--- Code/Main_Analysis.R
+++ Code/Main_Analysis.R
@@ -27,18 +27,18 @@ data <- data_preparation(data = data, thresholds = c(500, 1000, 2000, 6000),
 
 # Relative frequencies of travel distances over time:
 freq_periods(data)
-#ggsave("Graphics/Figure3.pdf", width = 10, height = 6)
+ggsave("Graphics/Figure3.pdf", width = 8, height = 4.5)
 
 # Individual distance curve for travelers in 2018:
 plot_ridgeline(data = data, period = 2018, weighted = TRUE)
-#ggsave("Graphics/Figure4.pdf", width = 10, height = 4.5)
+ggsave("Graphics/Figure4.pdf", width = 10, height = 4.5)
 
 # Ridgeline matrix:
 ridgeline_matrix(data = data, ages = list(20, 30, 40, 50, 60, 70, 80),
                  periods = list(1971:1979, 1980:1989, 1990:1999,
                                 2000:2009, 2010:2018),
                  log = TRUE) 
-#ggsave("Graphics/Figure5.pdf", width = 12, height = 8)
+ggsave("Graphics/Figure5.pdf", width = 11, height = 7.33)
 
 
 ################################################################################
@@ -51,7 +51,8 @@ data <- preprocessing_model(data)
 # Estimation of pure APC models:
 model_list_pure <- lapply(X = 1:5, FUN = function(i) {
   model <- model_pure(data = data, target = i, method = "gam")
-  #saveRDS(object = model, file = paste0("Models/Pure_Model_", i, ".rds"))
+  saveRDS(object = model,
+          file = paste0("Models/Main_Analysis/Pure_Model_", i, ".rds"))
   return(model)
 })
 
@@ -65,32 +66,63 @@ heatmaps <- lapply(X = model_list_pure, FUN = function(i) {
   plots <- plot_heatmap(model = i, data = data)
   return(plots)
 })
-#ggsave(plot = heatmaps[[1]], filename = "Graphics/Figure6.pdf", width = 17.5,
-#       height = 7)
+ggsave(plot = heatmaps[[5]], filename = "Graphics/Figure6.pdf", width = 17.5,
+       height = 7)
+for (i in 1:4) {
+  ggsave(plot = heatmaps[[i]],
+         filename = paste0("Graphics/A_Appendix/Figure", i, ".pdf"),
+         width = 17.5, height = 7)
+}
 
 # Visualization of marginal effects of all distance categories:
 plot_marginal_effects(model_list = model_list_pure, data = data, type = "pure")
-#ggsave("Graphics/Figure7.pdf", width = 10, height = 6)
+ggsave("Graphics/Figure7.pdf", width = 10, height = 6)
 
 # Visualization of partial APC plots:
+# Cohort:
+partial_plots_cohort <- lapply(X = 1:5, FUN = function(i) {
+  if (i < 5) {
+    title <- TRUE
+  }
+  else {
+    title <- FALSE
+  }
+  plots <- partial_APC_plots(model = model_list_pure[[i]], data = data,
+                             variable = "cohort", title = title)
+  return(plots)
+})
+ggsave(plot = partial_plots_cohort[[5]], filename = "Graphics/Figure8.pdf",
+       width = 10, height = 5)
+for (i in 1:4) {
+  ggsave(plot = partial_plots_cohort[[i]],
+         filename = paste0("Graphics/B_Appendix/Figure", i * 3 + 2, ".pdf"),
+         width = 10, height = 5)
+}
 # Age:
 partial_plots_age <- lapply(X = model_list_pure, FUN = function(i) {
   plots <- partial_APC_plots(model = i, data = data, variable = "age",
                              type = "pure")
   return(plots)
 })
+for (i in 1:4) {
+  ggsave(plot = partial_plots_age[[i]],
+         filename = paste0("Graphics/B_Appendix/Figure", i * 3 + 3, ".pdf"),
+         width = 10, height = 5)
+}
+ggsave(plot = partial_plots_age[[5]],
+       filename = "Graphics/B_Appendix/Figure17.pdf", width = 10, height = 5)
 # Period:
 partial_plots_period <- lapply(X = model_list_pure, FUN = function(i) {
   plots <- partial_APC_plots(model = i, data = data, variable = "period")
   return(plots)
 })
-# Cohort:
-partial_plots_cohort <- lapply(X = model_list_pure, FUN = function(i) {
-  plots <- partial_APC_plots(model = i, data = data, variable = "cohort")
-  return(plots)
-})
-#ggsave(plot = partial_plots_cohort[[5]], filename = "Graphics/Figure8.pdf",
-#       width = 12, height = 6)
+for (i in 1:4) {
+  ggsave(plot = partial_plots_period[[i]],
+         filename = paste0("Graphics/B_Appendix/Figure", i * 3 + 4, ".pdf"),
+         width = 10, height = 5)
+}
+ggsave(plot = partial_plots_period[[5]],
+       filename = "Graphics/B_Appendix/Figure18.pdf", width = 10, height = 5)
 
 
 ################################################################################
@@ -100,7 +132,8 @@ partial_plots_cohort <- lapply(X = model_list_pure, FUN = function(i) {
 # Estimation of covariate models:
 model_list_covariate <- lapply(X = 1:5, FUN = function(i) {
   model <- model_covariate(data = data, target = i, method = "gam")
-  #saveRDS(object = model, file = paste0("Models/Covariate_Model_", i, ".rds"))
+  saveRDS(object = model,
+          file = paste0("Models/Main_Analysis/Covariate_Model_", i, ".rds"))
   return(model)
 })
 
@@ -144,8 +177,8 @@ plots_covariates <- lapply(X = model_list_covariate, FUN = function(i) {
   plots <- plot_covariates(model = i, data = data)
   return(plots)
 })
-#ggsave(plot = plots_covariates[[5]], filename = "Graphics/Figure9.pdf",
-#       width = 10, height = 7)
+ggsave(plot = plots_covariates[[5]], filename = "Graphics/Figure9.pdf",
+       width = 10, height = 7)
 
 # Comparison of marginal effects between pure and covariate APC models:
 comparison_plots <- lapply(X = 1:5, FUN = function(i) {
@@ -154,8 +187,8 @@ comparison_plots <- lapply(X = 1:5, FUN = function(i) {
                                   data = data)
   return(plots)
 })
-#ggsave(plot = comparison_plots[[5]], filename = "Graphics/Figure10.pdf",
-#       width = 10, height = 6)
+ggsave(plot = comparison_plots[[5]], filename = "Graphics/Figure10.pdf",
+       width = 10, height = 5)
 
 
 ###############################################################################
--- Code/Sensitivity_Analysis.R
+++ Code/Sensitivity_Analysis.R
@@ -19,6 +19,7 @@ source("Code/Functions.R")
 ################################################################################
 
 # Data preparation:
+data <- readRDS("Data/data.rds")
 data <- data_preparation(data = data, thresholds = c(500, 1000, 2000, 6000),
                          labels = c("< 500 km", "500 - 1,000 km",
                                     "1,000 - 2,000 km", "2,000 - 6,000 km",
@@ -33,15 +34,18 @@ data_west <- data %>% filter(is.na(S_Herkunft) | S_Herkunft == "West")
 
 # Relative frequencies of travel distances over time:
 freq_periods(data_west)
+#ggsave("Graphics/C_Appendix/Figure19.pdf", width = 8, height = 4.5)
 
 # Individual distance curve for travelers in 2018:
 plot_ridgeline(data = data_west, period = 2018, weighted = TRUE)
+#ggsave("Graphics/C_Appendix/Figure20.pdf", width = 10, height = 4.5)
 
 # Ridgeline matrix:
 ridgeline_matrix(data = data_west, ages = list(20, 30, 40, 50, 60, 70, 80),
                  periods = list(1971:1979, 1980:1989, 1990:1999,
                                 2000:2009, 2010:2018),
-                 log = TRUE) 
+                 log = TRUE)
+#ggsave("Graphics/C_Appendix/Figure21.pdf", width = 11, height = 7.33)
 
 
 ################################################################################
@@ -54,6 +58,9 @@ data_west <- preprocessing_model(data_west)
 # Estimation of pure APC models:
 model_list_pure <- lapply(X = 1:5, FUN = function(i) {
   model <- model_pure(data = data_west, target = i, method = "gam")
+  #saveRDS(object = model,
+  #        file = paste0("Models/Sensitivity_Analysis/Pure_Model_West_", i,
+  #                      ".rds"))
   return(model)
 })
 
@@ -67,10 +74,13 @@ heatmaps <- lapply(X = model_list_pure, FUN = function(i) {
   plots <- plot_heatmap(model = i, data = data_west)
   return(plots)
 })
+#ggsave(plot = heatmaps[[5]], filename = "Graphics/C_Appendix/Figure22.pdf",
+#       width = 17.5, height = 7)
 
 # Visualization of marginal effects of all distance categories:
 plot_marginal_effects(model_list = model_list_pure, data = data_west,
                       type = "pure")
+#ggsave("Graphics/C_Appendix/Figure23.pdf", width = 10, height = 6)
 
 # Visualization of partial APC plots:
 # Age:
@@ -88,9 +98,11 @@ partial_plots_period <- lapply(X = model_list_pure, FUN = function(i) {
 # Cohort:
 partial_plots_cohort <- lapply(X = model_list_pure, FUN = function(i) {
   plots <- partial_APC_plots(model = i, data = data_west, variable = "cohort",
-                             type = "pure")
+                             type = "pure", title = FALSE)
   return(plots)
 })
+#ggsave(plot = partial_plots_cohort[[5]],
+#       filename = "Graphics/C_Appendix/Figure24.pdf", width = 10, height = 5)
 
 
 ################################################################################
@@ -100,6 +112,9 @@ partial_plots_cohort <- lapply(X = model_list_pure, FUN = function(i) {
 # Estimation of covariate models:
 model_list_covariate <- lapply(X = 1:5, FUN = function(i) {
   model <- model_covariate(data = data_west, target = i, method = "gam")
+  #saveRDS(object = model,
+  #        file = paste0("Models/Sensitivity_Analysis/Covariate_Model_West_", i,
+  #                      ".rds"))
   return(model)
 })
 
@@ -144,6 +159,8 @@ plots_covariates <- lapply(X = model_list_covariate, FUN = function(i) {
   plots <- plot_covariates(model = i, data = data_west)
   return(plots)
 })
+#ggsave(plot = plots_covariates[[5]],
+#       filename = "Graphics/C_Appendix/Figure25.pdf", width = 10, height = 7)
 
 # Comparison of marginal effects between pure and covariate APC models:
 comparison_plots <- lapply(X = 1:5, FUN = function(i) {
@@ -152,7 +169,8 @@ comparison_plots <- lapply(X = 1:5, FUN = function(i) {
                                   data = data_west)
   return(plots)
 })
-
+#ggsave(plot = comparison_plots[[5]],
+#       filename = "Graphics/C_Appendix/Figure26.pdf", width = 10, height = 5)
 
 
 ################################################################################
@@ -174,15 +192,18 @@ data_german <- data %>% filter(is.na(S_Staatsangehoerigkeit) |
 
 # Relative frequencies of travel distances over time:
 freq_periods(data_german)
+#ggsave("Graphics/C_Appendix/Figure27.pdf", width = 8, height = 4.5)
 
 # Individual distance curve for travelers in 2018:
 plot_ridgeline(data = data_german, period = 2018, weighted = TRUE)
+#ggsave("Graphics/C_Appendix/Figure28.pdf", width = 10, height = 4.5)
 
 # Ridgeline matrix:
 ridgeline_matrix(data = data_german, ages = list(20, 30, 40, 50, 60, 70, 80),
                  periods = list(1971:1979, 1980:1989, 1990:1999,
                                 2000:2009, 2010:2018),
-                 log = TRUE) 
+                 log = TRUE)
+#ggsave("Graphics/C_Appendix/Figure29.pdf", width = 11, height = 7.33)
 
 
 ################################################################################
@@ -195,6 +216,9 @@ data_german <- preprocessing_model(data_german)
 # Estimation of pure APC models:
 model_list_pure <- lapply(X = 1:5, FUN = function(i) {
   model <- model_pure(data = data_german, target = i, method = "gam")
+  #saveRDS(object = model,
+  #        file = paste0("Models/Sensitivity_Analysis/Pure_Model_German_", i,
+  #                      ".rds"))
   return(model)
 })
 
@@ -208,10 +232,13 @@ heatmaps <- lapply(X = model_list_pure, FUN = function(i) {
   plots <- plot_heatmap(model = i, data = data_german)
   return(plots)
 })
+#ggsave(plot = heatmaps[[5]], filename = "Graphics/C_Appendix/Figure30.pdf",
+#       width = 17.5, height = 7)
 
 # Visualization of marginal effects of all distance categories:
 plot_marginal_effects(model_list = model_list_pure, data = data_german,
                       type = "pure")
+#ggsave("Graphics/C_Appendix/Figure31.pdf", width = 10, height = 6)
 
 # Visualization of partial APC plots:
 # Age:
@@ -228,9 +255,11 @@ partial_plots_period <- lapply(X = model_list_pure, FUN = function(i) {
 # Cohort:
 partial_plots_cohort <- lapply(X = model_list_pure, FUN = function(i) {
   plots <- partial_APC_plots(model = i, data = data_german,
-                             variable = "cohort")
+                             variable = "cohort", title = FALSE)
   return(plots)
 })
+#ggsave(plot = partial_plots_cohort[[5]],
+#       filename = "Graphics/C_Appendix/Figure32.pdf", width = 10, height = 5)
 
 
 ################################################################################
@@ -240,6 +269,9 @@ partial_plots_cohort <- lapply(X = model_list_pure, FUN = function(i) {
 # Estimation of covariate models:
 model_list_covariate <- lapply(X = 1:5, FUN = function(i) {
   model <- model_covariate(data = data_german, target = i, method = "gam")
+  #saveRDS(object = model,
+  #        file = paste0("Models/Sensitivity_Analysis/Covariate_Model_German_",
+  #                      i, ".rds"))
   return(model)
 })
 
@@ -283,6 +315,8 @@ plots_covariates <- lapply(X = model_list_covariate, FUN = function(i) {
   plots <- plot_covariates(model = i, data = data_german)
   return(plots)
 })
+#ggsave(plot = plots_covariates[[5]],
+#       filename = "Graphics/C_Appendix/Figure33.pdf", width = 10, height = 7)
 
 # Comparison of marginal effects between pure and covariate APC models:
 comparison_plots <- lapply(X = 1:5, FUN = function(i) {
@@ -291,7 +325,8 @@ comparison_plots <- lapply(X = 1:5, FUN = function(i) {
                                   data = data_german)
   return(plots)
 })
-
+#ggsave(plot = comparison_plots[[5]],
+#       filename = "Graphics/C_Appendix/Figure34.pdf", width = 10, height = 5)
 
 ###############################################################################
 
